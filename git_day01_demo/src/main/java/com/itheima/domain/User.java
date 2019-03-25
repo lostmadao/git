569a6e92f53ae29a84759dd6d9c782f856fd14fb
lostmadao <lostmadao@yeah.net>
2019-03-25 11:08:39 +0800

    修改user.java,提交到本地仓库

--- git_day01_demo/src/main/java/com/itheima/domain/User.java
+++ git_day01_demo/src/main/java/com/itheima/domain/User.java
@@ -7,5 +7,6 @@ public class User {
     private String houtai; // 后台人员开发
     private String houtaireny; // 后台人员
     private String qianduan; // 前端人员
-    private Integer age;
+    private Integer age; //从业人员的年龄
+
 }
